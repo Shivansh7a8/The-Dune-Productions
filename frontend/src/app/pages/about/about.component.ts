94b3affc2d4fa0d882f5e19fe50a0f716d2bf914
Shivansh Shukla <89889494+Shivansh7a8@users.noreply.github.com>
2026-07-01 11:38:59 +0530

    Update team member names in about component

--- frontend/src/app/pages/about/about.component.ts
+++ frontend/src/app/pages/about/about.component.ts
@@ -134,13 +134,13 @@ export class AboutComponent {
       img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=400&auto=format&fit=crop",
     },
     {
-      name: "Arpita wani",
+      name: "Akshita Khanna",
       role: "Co-Founder, Dune Productions",
       alt: "photo",
       img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=400&auto=format&fit=crop",
     },
     {
-      name: "Abhishek kapoor",
+      name: "Abhishek B",
       role: "Co-Founder, Dunova Systems",
       alt: "photo",
       img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=400&auto=format&fit=crop",
